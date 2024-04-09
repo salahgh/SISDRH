3fbc8cc3acdf3453c20230e98da6cbb862bae7ea
Administrateur <sonyexperiasola29@gmail.com>
2024-04-09 11:32:01 +0200

    document types updated

---
+++ BACK_END/src/main/java/com/example/grh_n/scripts/sync_r_tables.sql
@@ -0,0 +1,5 @@
+
+select * from R_TYPE_TEXT_REGLEMENTAIRE ;
+insert into R_TYPE_TEXT_REGLEMENTAIRE values (7 , 'لائحة تذكيريى' , 'NOTE RAPPEL') ;
+insert into R_TYPE_TEXT_REGLEMENTAIRE values (8 , 'مشروع قرار' , 'PROJET DE LOIS') ;
+commit ;
