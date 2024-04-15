37c1b323906c2ece1118a0c67b9c1e766dd00b65
Administrateur <sonyexperiasola29@gmail.com>
2024-04-15 10:11:42 +0200

    inset new document type

--- BACK_END/src/main/java/com/example/grh_n/scripts/sync_r_tables.sql
+++ BACK_END/src/main/java/com/example/grh_n/scripts/sync_r_tables.sql
@@ -2,4 +2,8 @@
 select * from R_TYPE_TEXT_REGLEMENTAIRE ;
 insert into R_TYPE_TEXT_REGLEMENTAIRE values (7 , 'لائحة تذكيريى' , 'NOTE RAPPEL') ;
 insert into R_TYPE_TEXT_REGLEMENTAIRE values (8 , 'مشروع قرار' , 'PROJET DE LOIS') ;
+insert into R_TYPE_TEXT_REGLEMENTAIRE values (9 , 'محضر إجتماع' , 'PV_REUNION') ;
+insert into R_TYPE_TEXT_REGLEMENTAIRE values (10 , 'محضر' , 'PV') ;
+insert into R_TYPE_TEXT_REGLEMENTAIRE values (11 , 'DIRECTIVE' , 'DIRECTIVE') ;
+
 commit ;
