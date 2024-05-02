8106d2086daffb50eabbf6ce3f89ba252417b8c6
Administrateur <sonyexperiasola29@gmail.com>
2024-05-02 14:34:07 +0200

    TexteReglementaireNotesService and corresponding entities dtos and repos

---
+++ BACK_END/src/main/java/com/example/grh_n/textReglementaire/tess4j/OcrResultJPA/OcrResult/notes/TexteReglementaireNote.java
@@ -0,0 +1,33 @@
+package com.example.grh_n.textReglementaire.tess4j.OcrResultJPA.OcrResult.notes;
+
+import com.example.grh_n.security.user.User;
+import com.example.grh_n.textReglementaire.tess4j.OcrResultJPA.OcrResult.OcrResultEntityJpa;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class TexteReglementaireNote {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private String id;
+
+    @Column(length = 2000)
+    private String text ;
+
+    @ManyToOne
+    User owner ;
+
+    @ManyToOne
+    OcrResultEntityJpa textReglemetaire ;
+
+    private LocalDateTime dateCreation ;
+
+}
---
+++ BACK_END/src/main/java/com/example/grh_n/textReglementaire/tess4j/OcrResultJPA/OcrResult/notes/TexteReglementaireNotesRepository.java
@@ -0,0 +1,19 @@
+package com.example.grh_n.textReglementaire.tess4j.OcrResultJPA.OcrResult.notes;
+
+import com.example.grh_n.security.user.User;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
+
+public interface TexteReglementaireNotesRepository extends CrudRepository<TexteReglementaireNote, String> , PagingAndSortingRepository<TexteReglementaireNote,String> {
+
+    @Query("select text from TexteReglementaireNote text where text.owner.id = :ownerId")
+    Page<TexteReglementaireNote> getAllByOwner(String ownerId , Pageable pageable) ;
+
+    @Query("select text from TexteReglementaireNote text where text.textReglemetaire.id = :textId")
+    Page<TexteReglementaireNote> getAllByText(String textId , Pageable pageable) ;
+
+
+}
