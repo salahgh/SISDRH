673607c4cce868e7a453370189c4fc5fc2e745b0
Administrateur <sonyexperiasola29@gmail.com>
2024-05-03 17:42:33 +0200

    front end functionality added including graphql queries and mutations

--- FRONT_END/src/applications/textReglementaire/pages/PdfFile/PdfFile.tsx
+++ FRONT_END/src/applications/textReglementaire/pages/PdfFile/PdfFile.tsx
@@ -1,6 +1,128 @@
 import HOcrViewer from "../../../common/components/ocr/ocrSearchViewer/HOcrViewer.tsx";
+import {
+  Button,
+  IconButton,
+  List,
+  ListItem,
+  ListItemAvatar,
+  ListItemButton,
+  ListItemText,
+  Paper,
+  Stack,
+  TextField,
+  Typography,
+} from "@mui/material";
+import {
+  AddCircle,
+  NoteRounded,
+  SaveOutlined,
+  Undo,
+} from "@mui/icons-material";
+import { useMutation, useQuery } from "@apollo/client";
+import {
+  CreateTextReglementaireNoteDocument,
+  NotesByTextIdAndOwnerIdDocument,
+  UpdateTextNoteDocument,
+} from "../../../../_generated_gql_/graphql.ts";
+import { selectSelectedFileId } from "../../../../redux/features/elasticSearch/selectedResultLineSlice.ts";
+import { useSelector } from "react-redux";
+import useSnackBarNotifications from "../../../common/notifications/useSnackBarNotifications.tsx";
+import { selectLoggedInUser } from "../../../../redux/features/auth/userSlice.ts";
+import { useState } from "react";
 
 const PdfFile = () => {
+  const selectedFileId = useSelector(selectSelectedFileId);
+  const username = useSelector(selectLoggedInUser).matricule;
+  const [editMode, setEditMode] = useState<number>(-1);
+  const [value, setValue] = useState("");
+
+  const { data: notes } = useQuery(NotesByTextIdAndOwnerIdDocument, {
+    variables: {
+      userName: username,
+      ocrResultId: selectedFileId,
+      pageable: {
+        pageNumber: 0,
+        pageSize: 50,
+      },
+    },
+  });
+
+  const [updateNote, { loading, error }] = useMutation(UpdateTextNoteDocument, {
+    refetchQueries: [
+      {
+        query: NotesByTextIdAndOwnerIdDocument,
+        variables: {
+          userName: username,
+          ocrResultId: selectedFileId,
+          pageable: {
+            pageNumber: 0,
+            pageSize: 50,
+          },
+        },
+      },
+    ],
+  });
+
+  const [create] = useMutation(CreateTextReglementaireNoteDocument, {
+    refetchQueries: [
+      {
+        query: NotesByTextIdAndOwnerIdDocument,
+        variables: {
+          userName: username,
+          ocrResultId: selectedFileId,
+          pageable: {
+            pageNumber: 0,
+            pageSize: 50,
+          },
+        },
+      },
+    ],
+  });
+
+  const { handleShowInfoSnackBar, handleShowGraphQlErrorSnackBar } =
+    useSnackBarNotifications();
+  const handleAddNote = () => {
+    create({
+      variables: {
+        note: {
+          textReglemetaireId: selectedFileId,
+        },
+      },
+    })
+      .then(() => handleShowInfoSnackBar("created"))
+      .catch((e) => handleShowGraphQlErrorSnackBar(JSON.stringify(e)));
+  };
+
+  const handleChange = (event) => {
+    setValue(event.target.value);
+  };
+
+  function handleEditModeEnter(index: number) {
+    setEditMode(index);
+    setValue(notes?.notesByTextIdAndOwnerId?.content?.at(index)?.text);
+  }
+
+  function handleUpdateNote(index: number) {
+    updateNote({
+      variables: {
+        noteId: notes?.notesByTextIdAndOwnerId?.content?.at(index)?.id,
+        text: value,
+      },
+    })
+      .then(() => handleShowInfoSnackBar("updated"))
+      .catch((e) => handleShowGraphQlErrorSnackBar(JSON.stringify(e)));
+    setEditMode(-1);
+  }
+
+  function isDisabled(index: number) {
+    console.log(notes?.notesByTextIdAndOwnerId?.content?.at(index)?.text);
+    console.log(value);
+    console.log(
+      notes?.notesByTextIdAndOwnerId?.content?.at(index)?.text === value,
+    );
+    return notes?.notesByTextIdAndOwnerId?.content?.at(index)?.text === value;
+  }
+
   return (
     <div
       style={{
@@ -8,7 +130,88 @@ const PdfFile = () => {
       }}
       className={"p-1 overflow-clip bg-amber-600"}
     >
-      <HOcrViewer showGoToPdf={false} showDeletePdfFile={true} />
+      <Stack direction={"row"} height={"100%"} spacing={1}>
+        <Stack flex={2}>
+          <HOcrViewer showGoToPdf={false} showDeletePdfFile={true} />
+        </Stack>
+        <Stack flex={1} className={"bg-amber-200"}>
+          <Paper sx={{ height: "100%", p: 1 }}>
+            <Typography
+              variant={"h3"}
+              sx={{
+                padding: 1,
+                width: "100%",
+                textAlign: "center",
+                color: "#eb6c02",
+              }}
+              fontWeight={"bold"}
+            >
+              الملاحظات
+            </Typography>
+            <Button
+              variant={"contained"}
+              sx={{ width: "100%" }}
+              style={{ boxSizing: "border-box" }}
+              startIcon={<AddCircle sx={{ width: 30, height: 30 }}></AddCircle>}
+              onClick={handleAddNote}
+            >
+              <Typography variant={"h5"} fontWeight={"bold"} padding={1}>
+                إضافة ملاحظة
+              </Typography>
+            </Button>
+            <List>
+              {notes?.notesByTextIdAndOwnerId?.content?.map((item, index) => {
+                return (
+                  <ListItemButton
+                    onDoubleClick={() => handleEditModeEnter(index)}
+                    key={index}
+                  >
+                    <ListItemAvatar>
+                      <NoteRounded
+                        sx={{ height: 35, width: 35 }}
+                        color={"warning"}
+                      ></NoteRounded>
+                    </ListItemAvatar>
+                    {index === editMode ? (
+                      <Stack
+                        direction={"row"}
+                        spacing={1}
+                        alignItems={"center"}
+                      >
+                        <TextField
+                          value={value}
+                          onChange={handleChange}
+                        ></TextField>
+                        <IconButton
+                          sx={{ height: 50, width: 50 }}
+                          onClick={() => handleUpdateNote(index)}
+                          disabled={isDisabled(index)}
+                        >
+                          <SaveOutlined
+                            sx={{ height: 35, width: 35 }}
+                            color={isDisabled(index) ? "disabled" : "primary"}
+                          ></SaveOutlined>
+                        </IconButton>
+                        <IconButton
+                          onClick={() => setEditMode(-1)}
+                          sx={{ height: 50, width: 50 }}
+                        >
+                          <Undo
+                            sx={{ height: 35, width: 35 }}
+                            color={"secondary"}
+                          ></Undo>
+                        </IconButton>
+                      </Stack>
+                    ) : (
+                      <ListItemText>{item?.text}</ListItemText>
+                    )}
+                  </ListItemButton>
+                );
+              })}
+            </List>
+          </Paper>
+        </Stack>
+      </Stack>
     </div>
   );
 };
