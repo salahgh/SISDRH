b1353ef43793d5fcef9ac29944968a5324ebfc2e
Administrateur <sonyexperiasola29@gmail.com>
2024-05-03 17:46:20 +0200

    OcrResultUserGrantService

--- BACK_END/src/main/java/com/example/grh_n/security/user/OcrResultUserGrantService.java
+++ BACK_END/src/main/java/com/example/grh_n/security/user/OcrResultUserGrantService.java
@@ -1,16 +1,15 @@
 package com.example.grh_n.security.user;
 
-import com.example.grh_n.textReglementaire.tess4j.OcrResultJPA.OcrResult.OcrResultEntityJpa;
 import com.example.grh_n.textReglementaire.tess4j.OcrResultJPA.OcrResult.OcrResultService;
 import io.leangen.graphql.annotations.GraphQLMutation;
 import io.leangen.graphql.annotations.GraphQLQuery;
 import io.leangen.graphql.spqr.spring.annotations.GraphQLApi;
+import jakarta.transaction.Transactional;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -20,40 +19,44 @@ public class OcrResultUserGrantService {
 
     private final OcrResultUserGrantRepository ocrResultUserGrantRepository;
     private final UserService userService;
-    private final OcrResultService ocrResultService;
+//    private final OcrResultService ocrResultService;
     private final Logger logger = LoggerFactory.getLogger(this.getClass());
-    public OcrResultUserGrantService(OcrResultUserGrantRepository ocrResultUserGrantRepository, UserService userService, OcrResultService ocrResultService) {
+    public OcrResultUserGrantService(OcrResultUserGrantRepository ocrResultUserGrantRepository, UserService userService) {
         this.ocrResultUserGrantRepository = ocrResultUserGrantRepository;
         this.userService = userService;
-        this.ocrResultService = ocrResultService;
+//        this.ocrResultService = ocrResultService;
     }
 
     @GraphQLMutation(name = "changeOcrResultGrant")
     @Transactional
     public List<OcrResultUserGrant> changeOcrResultGrant(List<OcrResultUserGrantKeyInput> ocrResultUserGrantKeysInput , String ocrResultId) {
-        List<OcrResultUserGrantKey> resultUserGrantKeys = ocrResultUserGrantKeysInput.stream().map((item) -> OcrResultUserGrantKey.builder()
-                .OcrResultId(item.getOcrResultId())
-                .userId(item.getUserId())
-                .type(item.getType().getDisplayName())
-                .build()).toList();
-        OcrResultEntityJpa ocrResultEntityJpa = ocrResultService.findById(ocrResultId);
-        if ( ocrResultEntityJpa.getOcrResultUserGrants() != null )  ocrResultUserGrantRepository.deleteAll(ocrResultEntityJpa.getOcrResultUserGrants());
-        List<OcrResultUserGrant> grants = new java.util.ArrayList<>();
-        for (OcrResultUserGrantKey ocrResultUserGrantKey : resultUserGrantKeys) {
-            User user = userService.getByMatricule(ocrResultUserGrantKey.getUserId());
-            grants.add(ocrResultUserGrantRepository.save(
-                    OcrResultUserGrant.builder()
-                            .id(ocrResultUserGrantKey)
-                            .ocrResultEntityJpa(ocrResultEntityJpa)
-                            .user(user)
-                            .build()
-            ));
-        }
-        return grants;
+//        List<OcrResultUserGrantKey> resultUserGrantKeys = ocrResultUserGrantKeysInput.stream().map((item) -> OcrResultUserGrantKey.builder()
+//                .OcrResultId(item.getOcrResultId())
+//                .userId(item.getUserId())
+//                .type(item.getType().getDisplayName())
+//                .build()).toList();
+//        OcrResultEntityJpa ocrResultEntityJpa = ocrResultService.findById(ocrResultId);
+//        if ( ocrResultEntityJpa.getOcrResultUserGrants() != null )  ocrResultUserGrantRepository.deleteAll(ocrResultEntityJpa.getOcrResultUserGrants());
+//        List<OcrResultUserGrant> grants = new java.util.ArrayList<>();
+//        for (OcrResultUserGrantKey ocrResultUserGrantKey : resultUserGrantKeys) {
+//            User user = userService.getByMatricule(ocrResultUserGrantKey.getUserId());
+//            grants.add(ocrResultUserGrantRepository.save(
+//                    OcrResultUserGrant.builder()
+//                            .id(ocrResultUserGrantKey)
+//                            .ocrResultEntityJpa(ocrResultEntityJpa)
+//                            .user(user)
+//                            .build()
+//            ));
+//        }
+        return null;
     }
 
     @GraphQLQuery
     public Page<OcrResultUserGrant> getOcrResultUserGrantsPaged(Pageable pageable) {
         return ocrResultUserGrantRepository.findAll(pageable);
+    }
+
+    public  void deleteOcrResultGrants(List<OcrResultUserGrant> grants){
+        ocrResultUserGrantRepository.deleteAll(grants);
     }
 }
