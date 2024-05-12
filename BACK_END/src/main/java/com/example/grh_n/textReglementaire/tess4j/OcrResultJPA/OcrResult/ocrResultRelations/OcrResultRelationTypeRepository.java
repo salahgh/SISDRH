0e01b63e7b28489fc1933455f25475005cc487c1
Administrateur <sonyexperiasola29@gmail.com>
2024-05-12 18:17:30 +0200

    new created files

---
+++ BACK_END/src/main/java/com/example/grh_n/textReglementaire/tess4j/OcrResultJPA/OcrResult/ocrResultRelations/OcrResultRelationTypeRepository.java
@@ -0,0 +1,4 @@
+package com.example.grh_n.textReglementaire.tess4j.OcrResultJPA.OcrResult.ocrResultRelations;
+
+public interface OcrResultRelationTypeRepository extends org.springframework.data.repository.CrudRepository<com.example.grh_n.textReglementaire.tess4j.OcrResultJPA.OcrResult.ocrResultRelations.OcrResultRelationType, java.lang.String> {
+}
